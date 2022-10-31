87cb4d01b53364d620e8a411f40d74070daa4ce9
Tim <chibuiketimokonkwo@gmail.com>
2022-10-31 17:38:56 +0100

    deploy fixes

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,12 @@
 import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   mode: "development",
-//   build: {
-//     minify: false,
-//     minifySyntax: false
-//   }
-// })
-
 export default defineConfig({
-  plugins: [reactRefresh()],
-  mode: "development",
+  plugins: [react()],
+  mode: "production",
   build: {
     minify: false,
+    minifySyntax: false
   }
-});
+})
